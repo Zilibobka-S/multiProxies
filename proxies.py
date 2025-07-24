1052ea27f55d97a42c3241fb769c5bc4c6b05326
zilibobka <karelovm731@gmail.com>
2025-07-24 23:13:16 +0300

    Added selection between socks and http (user needs to change variable)

--- proxies.py
+++ proxies.py
@@ -1,14 +1,14 @@
-import requests, re                                                                                
-from bs4 import BeautifulSoup                                                                      
-                                                                                                   
-regex = r"([0-9]+(?:\.[0-9]+){3}:[0-9]+).*\+"                                                      
-regex_cleaner = r"[0-9]+(?:\.[0-9]+){3}:[0-9]+"                                                    
-c = requests.get("https://spys.me/socks.txt")                                                      
-test_str = c.text                                                                                  
-a = re.finditer(regex, test_str, re.MULTILINE)                                                     
-with open("proxies_list.txt", 'w') as file:                                                        
-    for i in a:                                                                                    
-       print(i.group(1),file=file)                                                                 
-                                                                                                   
-                                                                                           
-                                             
+import requests, re
+from bs4 import BeautifulSoup
+ptype = "socks.txt" #socks.txt for socks, proxy.txt for http
+regex = r"([0-9]+(?:\.[0-9]+){3}:[0-9]+).*\+"
+regex_cleaner = r"[0-9]+(?:\.[0-9]+){3}:[0-9]+"
+c = requests.get("https://spys.me/" + ptype)
+test_str = c.text
+a = re.finditer(regex, test_str, re.MULTILINE)
+with open("proxies_list.txt", 'w') as file:
+    for i in a:
+       print(i.group(1),file=file)
+
+
+
